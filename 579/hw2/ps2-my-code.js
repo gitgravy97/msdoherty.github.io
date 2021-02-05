4eba4bed17439eaa51574850e8f0cb4958de5de2
McCoy <mcshaned@umich.edu>
2021-02-05 10:22:50 -0500

    JS challenges finished

--- 579/hw2/ps2-my-code.js
+++ 579/hw2/ps2-my-code.js
@@ -10,6 +10,7 @@
  */
 
 function addOne(x) {
+  return(x+1);
 }
 
 
@@ -19,6 +20,11 @@ function addOne(x) {
  */
 
 function largest(lst) {
+  let large = lst[0];
+  for(i=0; i<lst.length; i++){
+    if(lst[i]>large){large=lst[i]}
+  }
+  return(large);
 }
 
 /**
@@ -33,6 +39,12 @@ function largest(lst) {
  */
 
 function fact(n) {
+  if(n===0|n===1){
+    return(1);
+  }
+  else{
+    return(n*fact(n-1));
+  }
 }
 
 /**
@@ -57,4 +69,10 @@ function fact(n) {
   */
 
 function fib(n) {
+  if(n===0|n===1){
+    return(n);
+  }
+  else{
+    return(fib(n-1)+fib(n-2));
+  }
 }
