d109f9baa4273ea63256fe43411416b706a36277
Mac <mcshaned@umich.edu>
2021-02-17 20:40:16 -0500

    Added checkboxes with listeners to toggle display of elements on/off, hide/unhide octave headers, added generator for custom beep based on user input frequency, added beep-or-keep system that can save custom beeps, added purge for custom beep section

--- 579/hw4/audio.js
+++ 579/hw4/audio.js
@@ -1,6 +1,7 @@
-var context = new AudioContext()
 
 function beep(osc_type, pitch){
+    var context = new AudioContext()
+
     var o = context.createOscillator()
     var g = context.createGain()
     o.type = osc_type
@@ -12,4 +13,67 @@ function beep(osc_type, pitch){
 
     g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 3)
     o.start()
+}
+
+function inputBeep(){
+    // console.log(document.getElementById("PitchInput").value)
+    beep(osc_type="sine", pitch=document.getElementById("PitchInput").value)
+}
+
+// https://stackoverflow.com/questions/50460645/getelementbyid-style-display-does-not-work
+function toggleViz(element){
+    element.classList.toggle("hidden");
+}
+
+function toggleOctLabels(){
+    // https://stackoverflow.com/questions/14810506/map-function-for-objects-instead-of-arrays
+    targets = document.getElementsByClassName('octave');
+    //Object.keys(targets).map(each => console.log(each))
+    //Object.keys(targets).map(each => console.log(targets[each]))
+    Object.keys(targets).map(each => toggleViz(targets[each].getElementsByTagName('h2')[0]))
+    //targets.map(each => console.log(each));
+}
+
+const toggle_oct_cust = document.getElementById('tog_oct_cust');
+const toggle_oct3 = document.getElementById('tog_oct3');
+const toggle_oct4 = document.getElementById('tog_oct4');
+const toggle_oct5 = document.getElementById('tog_oct5');
+
+toggle_oct_cust.addEventListener('click', ()=>toggleViz(document.getElementById('keeped_beeps')));
+toggle_oct3.addEventListener('click', ()=>toggleViz(document.getElementById('oct3')));
+toggle_oct4.addEventListener('click', ()=>toggleViz(document.getElementById('oct4')));
+toggle_oct5.addEventListener('click', ()=>toggleViz(document.getElementById('oct5')));
+
+const toggle_oct_labels = document.getElementById('oct_relabel');
+toggle_oct_labels.addEventListener('click', ()=>toggleOctLabels());
+
+const customPitchButton = document.getElementById('CustomPitchButton');
+customPitchButton.addEventListener('click', ()=>inputBeep());
+
+const customPitchEnter = document.getElementById('PitchInput');
+customPitchEnter.addEventListener('keydown', event => {
+    if(event.key == "Enter"){
+        inputBeep();
+    }
+});
+
+function BeepKeep(){
+    const keptPitch = document.getElementById('PitchInput').value
+    // console.log(keptPitch)
+    let keptBeep = document.createElement('button');
+    keptBeep.innerText = keptPitch;
+    keptBeep.className = "key white-key";
+    keptBeep.onclick = ()=>beep('sine', keptPitch);
+    document.getElementById('keeped_beeps').append(keptBeep);
+}
+
+const beep_creep = document.getElementById('SavePitchButton');
+beep_creep.addEventListener('click', ()=>BeepKeep())
+
+function PurgePitches(){
+    purgeables = document.getElementById('keeped_beeps').getElementsByClassName('key')
+    console.log(purgeables)
+    while(purgeables[0]){
+        purgeables[0].remove();
+    }
 }
