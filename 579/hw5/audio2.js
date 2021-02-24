3092ca148951c3f3e744a478fb219d8b7ebdd22f
Mac <mcshaned@umich.edu>
2021-02-24 13:34:06 -0500

    trying some stuff

--- 579/hw5/audio2.js
+++ 579/hw5/audio2.js
@@ -1,10 +1,13 @@
-function testo(){
-    console.log("BOOM");
-    var voice = new Pizzicato.Sound({ source: 'input' });
+function setMic(){
+    let zeet = true;
+    console.log(zeet);
+}
+function cutMic(){
+    zeet = true;
+    console.log(zeet);
 }
 
-const please_work = document.getElementById('please');
-// please_work.addEventListener('click', ()=>BeepKeep())
+const please_work = document.getElementById('microphone-feed');
 
 please_work.addEventListener('click', ()=>{
     console.log("rip");
@@ -14,4 +17,10 @@ please_work.addEventListener('click', ()=>{
     }, function() {
         voice.play();
     });
+
+    const please_stop = document.getElementById('microphone-cut');
+    please_stop.addEventListener('click'), ()=>{
+        console.log("kill trigger");
+        voice.stop();
+    }
 })
